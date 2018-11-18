5be6e7af3fc076467aea833e22861b9ffe7f0c57
Samuel Dufresne <samuel.dufresne@live.com>
2018-11-18 16:52:16 -0500

    Session file

--- client/src/feed/feed.js
+++ client/src/feed/feed.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import Publish from "./publish";
 import Post from "./post";
+import Session from "../session/session";
 import "./style/feed.css";
 
 class Feed extends Component {
   constructor(props) {
     super(props);
 
-    if (document.cookie.indexOf("user=") < 0) {
-      window.location = "/login/";
-    }
+    Session.checkSession();
   }
 
   render() {
--- client/src/session/session.js
+++ client/src/session/session.js
@@ -1,4 +1,12 @@
 function dropCookie(name, content) {
-    var str = name+"="+content;
-    document.cookie(str);
+  var str = name + "=" + content;
+  document.cookie(str);
 }
+
+function checkSession() {
+  if (document.cookie.indexOf("user=") < 0) {
+    window.location = "/login/";
+  }
+}
+
+export default { dropCookie, checkSession };
